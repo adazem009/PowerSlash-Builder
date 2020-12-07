f5b7ad5c5dc1f8e854ab218565dd0b6dd92b2071
adazem009 <68537469+adazem009@users.noreply.github.com>
2020-12-07 15:46:04 +0100

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -2,9 +2,10 @@
 # Example: git clone --single-branch --branch <branch> <URL to your repository> <path>
 # $SRC is the PowerSlash-Builder source tree path.
 SRC="$(dirname $BASH_SOURCE)"
-if ! [ -d "${SRC}/build" ]; then
-  mkdir "${SRC}/build"
+if [ -d "${SRC}/build" ]; then
+  rm -rf "${SRC}/build"
 fi
+mkdir "${SRC}/build"
 
 # Required dependencies - do NOT remove them!
 git clone https://github.com/adazem009/PowerSlash "$SRC/PowerSlash"
